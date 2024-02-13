6f86310812afa7a5590243422b127d531da717b4
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-02-13 14:46:24 +0200

    add controller

---
+++ UploadingDocxAPI/UploadingDocxAPI/Controllers/AttachmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UploadingDocxAPI.Services;
+
+namespace UploadingDocxAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AttachmentController : ControllerBase
+{
+    private readonly AzureBlobService _service;
+
+    public AttachmentController(AzureBlobService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        var response = await _service.UploadFiles(file);
+        return Ok(response);
+    } 
+}
