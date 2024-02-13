66b919aee484b8fca068d350071b811b7cea32c0
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-02-13 14:16:39 +0200

    add service for uploading files

---
+++ UploadingDocxAPI/UploadingDocxAPI/Services/AzureBlobService.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Storage.Blobs;
+
+namespace UploadingDocxAPI.Services;
+
+public class AzureBlobService
+{
+    private readonly  BlobServiceClient _blobCliet;
+    private readonly BlobContainerClient _containerClient;
+    
+    public AzureBlobService(BlobServiceClient blobCliet,IConfiguration configuration)
+    {
+        _blobCliet = blobCliet;
+        _containerClient = _blobCliet.GetBlobContainerClient(configuration.GetValue<string>("BlobContainerName"));
+    }
+
+    public async Task UploadFiles(IFormFile file)
+    {
+        BlobClient client = _containerClient.GetBlobClient(file.FileName);
+        await using (MemoryStream data = new MemoryStream())
+        {
+            file.CopyTo(data);
+            data.Position = 0;
+            await _containerClient.UploadBlobAsync(file.FileName, data, default);
+        }
+
+    }
+}
--- UploadingDocxAPI/UploadingDocxAPI/obj/Debug/net8.0/UploadingDocxAPI.AssemblyInfo.cs
+++ UploadingDocxAPI/UploadingDocxAPI/obj/Debug/net8.0/UploadingDocxAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UploadingDocxAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dea21ea7b69cebdcbb723aea442e25e39dd228a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e9374566c1a4f67090a822992bef60333a5a8a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UploadingDocxAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UploadingDocxAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
