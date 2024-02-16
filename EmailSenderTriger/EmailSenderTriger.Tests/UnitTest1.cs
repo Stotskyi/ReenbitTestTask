b8da871852f12056c0717c5e3316e753e994966e
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-02-16 14:43:43 +0200

    init project for testing EmailSenderTriger

---
+++ EmailSenderTriger/EmailSenderTriger.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Azure.Storage.Blobs;
+using FakeItEasy;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Logging.Abstractions;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace EmailSenderTriger.Tests;
+
+public class UnitTest1
+{
+   
+}
